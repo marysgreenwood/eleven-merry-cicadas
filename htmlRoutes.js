4eed61b2256b8cd197a4b56a96c78ffe1327886b
marysgreenwood <marysgreenwood@gmail.com>
2022-12-01 18:29:30 -0500

    debugged html routes

--- htmlRoutes.js
+++ htmlRoutes.js
@@ -4,12 +4,12 @@ const router= require('express').Router();
 
 //"/notes" returns the notes.html file
 router.get('/notes', (req, res) => {
-    res.sendFile(path.join(_dirname, './public/notes.html'));
+    res.sendFile(path.join(__dirname, './public/notes.html'));
 });
 //Other routes return the index.html file
 
 router.get('*', (req, res) => {
-    res.sendFile(path.join(_dirname, './public/index.html'))
+    res.sendFile(path.join(__dirname, './public/index.html'))
 });
 //exports routes for use by server.js
 module.exports = router;
